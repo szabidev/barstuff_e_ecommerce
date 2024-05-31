6fd29d086e2d693d69af0c5d33054b2d078945c4
szabidev <szabi.vojtek@gmail.com>
2024-05-31 22:52:18 +0300

    Displayed data from json

--- app/app-provider.tsx
+++ app/app-provider.tsx
@@ -1,119 +1,132 @@
-'use client'
+"use client";
 
-import {createContext, Dispatch, SetStateAction, useContext, useEffect, useState} from 'react'
-import {me} from "@/api/auth.api";
-import {removeKey} from "@/util/storage";
-import {useRouter} from "next/navigation";
-import { fetchDrinks } from '@/api/drinks';
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { me } from "@/api/auth.api";
+import { removeKey } from "@/util/storage";
+import { useRouter } from "next/navigation";
+import { fetchDrinks } from "@/api/drinks";
 // import {fetchProjects} from "@/api/projects.api";
 
 interface ContextProps {
-    sidebarOpen: boolean
-    setSidebarOpen: Dispatch<SetStateAction<boolean>>,
-    user: any,
-    clearUser: any,
-    fetchUser: any,
+  sidebarOpen: boolean;
+  setSidebarOpen: Dispatch<SetStateAction<boolean>>;
+  user: any;
+  clearUser: any;
+  fetchUser: any;
 
-    // projects: any,
-    drinks:Drink[],
+  // projects: any,
+  drinks: Drink[];
 }
 
 export interface Drink {
-    brand_name: string;
-    note: {
-        abv: string;
-        country: string;
-        manufacturer: string;
-    },
-    description: string
+  title: string;
+  image: string;
+  properties: {
+    country: string;
+    category: string;
+    tasting_notes?: string;
+    food_pairing?: string;
+    abv: string;
+  };
+  description: string;
 }
 
 const AppContext = createContext<ContextProps>({
-    sidebarOpen: false,
-    setSidebarOpen: (): boolean => false,
+  sidebarOpen: false,
+  setSidebarOpen: (): boolean => false,
 
-    user: null,
-    clearUser: () => null,
+  user: null,
+  clearUser: () => null,
 
-    fetchUser: null,
+  fetchUser: null,
 
-    // projects: [],
-    drinks:[],
-})
+  // projects: [],
+  drinks: [],
+});
 
-export default function AppProvider({children,}: {
-    children: React.ReactNode
+export default function AppProvider({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    const [sidebarOpen, setSidebarOpen] = useState<boolean>(false)
-    const router = useRouter()
-    const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    const [user, setCurrentUser] = useState<any>(null);
-    // const [projects, setProjects] = useState<any>([]);
-    const [drinks,setDrinks]=useState<Drink[]>([])
+  const [user, setCurrentUser] = useState<any>(null);
+  // const [projects, setProjects] = useState<any>([]);
+  const [drinks, setDrinks] = useState<Drink[]>([]);
 
+  const fetchUser = async () => {
+    const response = await me();
+    if (response.status === 401) {
+      await removeKey("token");
+      setCurrentUser(null);
 
-    const fetchUser = async () => {
-        const response = await me();
-        if (response.status === 401) {
-            await removeKey('token');
-            setCurrentUser(null);
-
-            setTimeout(() => {
-                router.replace('/signin');
-            }, 1000)
-        }
-
-        if (response.status === 200) {
-            setIsLoading(false);
-            const user = await response.json();
-            setCurrentUser(user);
-        } else {
-            const responseJSON = await response.json();
-            console.error(responseJSON);
-        }
-
-        setIsLoading(false);
-    };
-
-    // const handleProjects = async () => {
-    //     const data = await fetchProjects();
-    //     setProjects(data);
-    // };
-
-    const handleDrinks = async ()=> {
-        const data = await fetchDrinks();
-        setDrinks(data)
+      setTimeout(() => {
+        router.replace("/signin");
+      }, 1000);
     }
 
-    const clearUser = () => {
-        setCurrentUser(null);
+    if (response.status === 200) {
+      setIsLoading(false);
+      const user = await response.json();
+      setCurrentUser(user);
+    } else {
+      const responseJSON = await response.json();
+      console.error(responseJSON);
     }
 
-    useEffect(() => {
-        fetchUser();
-        // handleProjects();
-        handleDrinks()
-    }, []);
+    setIsLoading(false);
+  };
 
-    return (
-        <AppContext.Provider value={{
-            // Sidebar
-            sidebarOpen,
-            setSidebarOpen,
+  // const handleProjects = async () => {
+  //     const data = await fetchProjects();
+  //     setProjects(data);
+  // };
 
-            // Auth
-            user,
-            fetchUser,
-            clearUser,
+  const handleDrinks = async () => {
+    const data = await fetchDrinks();
+    setDrinks(data);
+  };
 
-            // Customize...
-            // projects
-            drinks
-        }}>
-            {children}
-        </AppContext.Provider>
-    )
+  const clearUser = () => {
+    setCurrentUser(null);
+  };
+
+  useEffect(() => {
+    fetchUser();
+    // handleProjects();
+    handleDrinks();
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{
+        // Sidebar
+        sidebarOpen,
+        setSidebarOpen,
+
+        // Auth
+        user,
+        fetchUser,
+        clearUser,
+
+        // Customize...
+        // projects
+        drinks,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 }
 
-export const useAppProvider = () => useContext(AppContext)
+export const useAppProvider = () => useContext(AppContext);
